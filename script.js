2243851722f16ab2da1478d43b5af9dfce2418ef
reyhanawaludin <rrqramadhann1@gmail.com>
2019-01-15 08:52:00 +0700

    kumpulan tugas

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// what is java script...
+
+// function Raehan(name) {
+//     this.name = name;
+// }
+// const raehan = new Raehan('Reyhan')
+
+// ############
+
+// belajar object..
+
+// const person = {
+//     name: 'reyhan',
+//     height: 170,
+//     age: 16,
+//     walk(){
+
+//     }
+ 
+// }
+// person.walk();
+// person.name= '';
+
+// const targetMember = 'name';
+// person[targetMember.value] = 'reyhan';
+
+//binding
+// const person = {
+//     name: "mosh",
+//     walk() {
+//         console, log(this);
+
+//     }
+
+// }
+// person.walk();
+
+// const walk = person.walk.bind(person);
+// walk();
+// console.log(walk);
+
+//arrow functions
+
+// const jobs = [
+//         { id: 1, isActive: true},
+//         { id: 2, isActive: true},
+//         { id: 3, isActive: true},
+        
+// ];
+
+// const activeJobs = jobs.filter(function(job) { return job.isActive;});
+// const activeJobs = jobs.filter(job => job.isActive);
+
+//basic constants
+
+// const interrestRate = 0.3;
+// interrestRate = 1;
+// console.log(interrestRate);
